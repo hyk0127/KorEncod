6a8319780aa7a58d64ad6e277ebffa5432f4f39e
YongKi Hong <hyk0127@gmail.com>
2021-02-26 20:22:44 -0500

    Fix existing error from hello.Rd

--- R/hello (DESKTOP-0AN6NT0's conflicted copy 2021-02-25).R
+++ R/hello (DESKTOP-0AN6NT0's conflicted copy 2021-02-25).R
@@ -1,9 +1,9 @@
 # sampleprob
-#' @import rvest
+#' @importFrom rvest html_text
+#' @importFrom xml2 read_html
 #' @param url sample URL from DART system
 #' @export
-sampleprob <- function(url) {
-  # sample url: "http://dart.fss.or.kr/dsaf001/main.do?rcpNo=20200330003851"
+sampleprob <- function(url = "http://dart.fss.or.kr/dsaf001/main.do?rcpNo=20200330003851") {
   result <- grepl("연결재무제표 주석", html_text(read_html(url)))
   return(result)
 }
